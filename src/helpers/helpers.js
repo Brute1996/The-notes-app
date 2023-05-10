116bd104f86f735cf4602e6ba6ecf7ca80caec84
Sergey <sergey.cherenkov96@gmail.com>
2023-05-10 23:48:13 +0300

    Delete helpers file

--- src/helpers/helpers.js
+++
@@ -1,7 +0,0 @@
-export const innerTextFormater = (maxLength, textValue = "") => {
-  if (textValue.length >= maxLength) {
-    return `${textValue.slice(0, maxLength)}...`;
-  } else {
-    return textValue;
-  }
-};
